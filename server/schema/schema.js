d0ff0edc4359f3bc4eabbd76d13a2e8b2019910f
Moaz Khan <moaz.khan@teamo.io>
2019-09-30 12:37:02 +0500

    find books using id

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,14 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// dummy data
+var books = [
+    {name:'Game of Thrones', genre: 'Action', id: '1'},
+    {name:'Harry Potter', genre: 'Fiction', id: '2'},
+    {name:'Rise and Shine', genre: 'Funny', id: '3'},
+]
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -20,6 +28,7 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLString }},
             resolve(parent, args){
                 // code to get data from db / other source
+                return _.find(books, {id: args.id})
             }
         }
     }
